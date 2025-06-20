facab8bcba961ba5879d5dec2f07d8a4a128f5a5
Ismail Mohamed <ismail.majeeb.02@gmail.com>
2025-06-20 04:36:49 +0300

    Minor Changes in Polybar, Nitrogen Configs

--- polybar/hack/launch.sh
+++ polybar/hack/launch.sh
@@ -10,6 +10,44 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q top -c "$DIR"/config.ini &
-polybar -q bottom -c "$DIR"/config.ini &
+desktop=$(echo $DESKTOP_SESSION)
+
+case $desktop in
+i3)
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            if [ $m == 'eDP1' ]; then
+                MONITOR=$m polybar --reload -q top -c "$DIR"/config.ini &
+                MONITOR=$m polybar --reload -q bottom -c "$DIR"/config.ini &
+            elif [ $m == 'HDMI1' ]; then
+                MONITOR=$m polybar --reload -q top -c "$DIR"/config.ini &
+                MONITOR=$m polybar --reload -q bottom -c "$DIR"/config.ini &
+            else
+                MONITOR=$m polybar --reload -q top -c "$DIR"/config.ini &
+                MONITOR=$m polybar --reload -q bottom -c "$DIR"/config.ini &
+            fi
+        done
+    else
+        MONITOR=$m polybar --reload -q top -c "$DIR"/config.ini &
+        MONITOR=$m polybar --reload -q bottom -c "$DIR"/config.ini &
+    fi
+    ;;
+openbox)
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload mainbar-openbox -c ~/.config/polybar/config &
+        done
+    else
+        polybar --reload mainbar-openbox -c ~/.config/polybar/config &
+    fi
+    ;;
+bspwm)
+    if type "xrandr" >/dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
+        done
+    else
+        polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
+    fi
+    ;;
+esac
